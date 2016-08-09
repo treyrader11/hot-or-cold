cc3c79aeffc1ad7f3a6c2dabf59216e7e98f2569
Trey Rader <treyrdr09@gmail.com>
2016-08-09 01:12:06 -0500

    code is cleaned up and kinks worked out

--- js/script.js
+++ js/script.js
@@ -8,28 +8,14 @@ $(document).ready(function() {
 	var guessButton = $('#guessButton');
 	var listOfGuesses = $('#guessList');
 	var guessCount = 0;
-	var secretNumber = 0;
- 
+	//following var contains an invoked function that is called when the page loads
+	var secretNumber = generateNumber(1,100);
 
-	      
-
-
+	console.log("The secretNumber is currently " + secretNumber);      
 	
-
-// Starts a new game via clicking .new and triggering newGame function
-
-    newGameButton.click(newGame);
-
-
-// When a user submits a guess via clicking the button with #guessButton, the function validNumber will be triggered
-
-    guessButton.click(inputNumber);
-
-
-
-
-
-    userInput.keypress(function(event) {
+	newGameButton.click(newGame);
+	guessButton.click(inputNumber);
+	userInput.keypress(function(event) {
     	var keycode = event.which;                       //var keycode = (event.keyCode ? event.keyCode : event.which)
     	if (keycode == 13) {
     		inputNumber();
@@ -37,20 +23,13 @@ $(document).ready(function() {
     });
 
  
-
-
-
-
-
-	function generateNumber(minimum, maximum) {
-		return Math.floor((Math.random() * maximum) + minimum);
-		
+	function generateNumber(min, max) {
+		return Math.floor((Math.random() * max) + min); 
+		//'max' is the highest number, min is the lowest it can be
 	};
 
-
-
 // A new game will start when clicking .new and will reset span#count, #userGuess, #feedback, and removing .newGuess. A secret number between 1 and 100 will be generated.
-
+//this function also controls the winning modal's newButton.
 	function newGame() {
 		console.log('***NEW GAME STARTED***');
 		//count resets to zero	
@@ -68,22 +47,10 @@ $(document).ready(function() {
 		// The function generateNumber will be called and will be put in a secretNumber variable
 		secretNumber = generateNumber(1,100);
 		console.log("the new secretNumber is " + secretNumber);
-		//userGuessFeedback(secretNumber, userGuess);
 	   };
 
-
-
-
-
-        
-    
-
-
-
-		
 	  //feedback will reflect in the feedbackHeader to inform user of how hot/cold the user is from the secretNumber.
 	function inputNumber() {
-		secretNumber = generateNumber(1,100);
 		number = userInput.val(); 
 		userGuess = parseInt(number);
 		console.log("your number is " + userGuess);
@@ -91,89 +58,64 @@ $(document).ready(function() {
 		if (userGuess >= 1 && userGuess <= 100) {
 			increaseCount();
 			generateFeedback(secretNumber, userGuess);
-			userInput.val('');
+			userInput.val('').focus();
+			//binding the input value to "focus()" allows for the input to gain focus right after inserting the button!
 		}
 		else {
 			alert("Insert a number between 1-100.")
-			userInput.val('');
+			userInput.val('').focus();
 		}
-	}
+	};
 
-
-	
-
-
+	//Everytime you make a guess this function is called, which increments by one
 	function increaseCount() {
 		guessCount++;
 		count.text(guessCount);
-	}	
+	};	
 
-
-
-
-
-
-
+	//this function deals with calculating the quantitative distances between the random number and user's guess
 	function generateFeedback(secretNumber, userGuess) {
 		var userFeedback = secretNumber - userGuess;
-		console.log("Your number is still " + userGuess);
-		console.log("The secretNumber is still " + secretNumber);
+		console.log("***Your number is still " + userGuess + "***");
+		console.log("***The secretNumber is still " + secretNumber + "***");
 		if (userFeedback === 0) {
 		 	feedbackHeader.text("The Secret Number is " + secretNumber + "!");
 		 	listOfGuesses.append('<li class="secret newGuess">' + userGuess + '</li>');
-		 	feedbackHeader.removeClass();
-		 	feedbackHeader.addClass('secret');
+		 	$('#winningModal').fadeIn(1000);
+		 	newGameButton.click(function() {
+		 		$('#winningModal').fadeOut(1000);
+		 	});
 		}
 		else if (userFeedback <= 9 && userFeedback >= -9) {
 		 	//between -9 to 9
 		 	feedbackHeader.text("Very Hot");
 		 	listOfGuesses.append('<li class="very-hot newGuess">' + userGuess + '</li>');
-		 	feedbackHeader.addClass('very-hot');
 		}
 		else if (userFeedback >= 10 && userFeedback <= 20 || userFeedback <= -10 &&  userFeedback >= -20)  {
 		 	// between 10 and 20 or between -10 and -20
 		 	feedbackHeader.text("Hot");
 		 	listOfGuesses.append('<li class="hot newGuess">' + userGuess + '</li>');
 		 	feedbackHeader.removeClass();
-		 	feedbackHeader.addClass('hot')
-		}
+		 	}
 		else if (userFeedback >= 21 && userFeedback <= 30 || userFeedback <= -21 && userFeedback >= -30) { 
 		 	//between 21 and 30 or between -21 and -30
 		 	feedbackHeader.text("Warm");
 		 	listOfGuesses.append('<li class="warm newGuess">' + userGuess + '</li>');
 		 	feedbackHeader.removeClass();
-            feedbackHeader.addClass('warm');
 		}
 		else if (userFeedback >= 31 && userFeedback <= 49 || userFeedback <= -31 && userFeedback >= -49) {
 			// between 31 and 49 or between -31 and -49
 			feedbackHeader.text("Cold");
 			listOfGuesses.append('<li class="cold newGuess">' + userGuess + '</li>');
 			feedbackHeader.removeClass();
-            feedbackHeader.addClass('cold');
 		}
 		else {
 			// 50/-50 or further away
 			feedbackHeader.text("Ice-Cold");
 			listOfGuesses.append('<li class="ice-cold newGuess">' + userGuess + '</li>');
-			feedbackHeader.removeClass();
-			feedbackHeader.addClass('ice-cold');
 		}
 
-	}
-
-
- 
-
-
-
-
-
-
-
-
- 	
-
-
+	};
 
 	$('.what').click(function() {
 		$(".overlay").fadeIn(1000);
@@ -183,3 +125,5 @@ $(document).ready(function() {
 	})
 
 })
+ 
+
